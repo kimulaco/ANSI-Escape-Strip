4542e7ab88b9ac70a0d216417b452aeb96178bd4
kimulaco <kmrkdev@gmail.com>
2020-01-09 00:56:21 +0900

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import About from './pages/About'
 import AppHeader from './components/AppHeader/AppHeader'
 import AppMain from './components/AppMain/AppMain'
 import AppFooter from './components/AppFooter/AppFooter'
+import './App.css'
 
 const App: React.FC = () => {
   return (
--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './AppHeader.css'
 
 const AppHeader: React.FC = () => {
   return (
     <header className="AppHeader">
       <div className="AppHeader_inner">
-        <h1 className="AppHeader_title">ANSI Strip Online</h1>
+        <h1 className="AppHeader_title">
+          <Link className="AppHeader_title-link" to="/">ANSI Strip Online</Link>
+        </h1>
+        <p className="AppHeader_about">
+          <Link className="AppHeader_about-link" to="/about">About</Link>
+        </p>
       </div>
     </header>
   )
