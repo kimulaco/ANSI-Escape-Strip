4ea6e8dbf95d85fcdfd078010942fd53468e696d
kimulaco <kmrkdev@gmail.com>
2020-01-09 21:20:44 +0900

    Add default ansi

--- src/components/FormTextarea/FormTextarea.tsx
+++ src/components/FormTextarea/FormTextarea.tsx
@@ -2,9 +2,11 @@ import React, { useState } from 'react'
 import stripAnsi from 'strip-ansi'
 import './FormTextarea.css'
 
+const DEFAULT_VALUE = '[31mH[39m[36me[39m[33ml[39m[32ml[39m[35mo[39m [31mW[39m[36mo[39m[33mr[39m[32ml[39m[35md[39m[31m![39m'
+
 const FormTextarea: React.FC = () => {
-  const [value, setValue] = useState('')
-  const [preview, setPreview] = useState(value)
+  const [value, setValue] = useState(DEFAULT_VALUE)
+  const [preview, setPreview] = useState(stripAnsi(DEFAULT_VALUE))
 
   const handleChange = (event: any): void => {
     setValue(event.target.value)
